08bcbaf9118cc6d1d6340f9da6ca7fdba11b57a1
Ben <bjeffrey92@hotmail.com>
2021-06-09 15:14:09 +0100

    implemented class which trains an HMM on proteins of each MIC and makes predictions based on HMM profile score

--- fit_HMM_model.py
+++ fit_HMM_model.py
@@ -3,7 +3,10 @@
 import argparse
 import logging
 
-from utils import load_data
+from sklearn.metrics import mean_squared_error
+
+from models.HMM_model import ProfileHMMPredictor
+from utils import accuracy, mean_acc_per_bin, load_data, ResultsContainer
 
 
 def parse_args():
@@ -54,6 +57,8 @@ def main(
 ):
     logging.info("Loading data")
 
+    pbp_seqs = ["a1_seq", "b2_seq", "x2_seq"]
+
     train, test_1, test_2, val = load_data(
         train_data_population,
         test_data_population_1,
@@ -61,6 +66,68 @@ def main(
         standardise_training_MIC,
         standardise_test_and_val_MIC,
     )
+
+    logging.info("Fitting model")
+    p_HMM_predictor = ProfileHMMPredictor(
+        train,
+        pbp_seqs=pbp_seqs,
+    )
+
+    train_y = train.log2_mic.values
+    test_1_y = test_1.log2_mic.values
+    test_2_y = test_2.log2_mic.values
+    val_y = val.log2_mic.values
+
+    logging.info("Generating predictions")
+    train_predictions = p_HMM_predictor.predict_phenotype(
+        train[pbp_seqs].sum(axis=1)
+    )
+    test_predictions_1 = p_HMM_predictor.predict_phenotype(
+        test_1[pbp_seqs].sum(axis=1)
+    )
+    test_predictions_2 = p_HMM_predictor.predict_phenotype(
+        test_2[pbp_seqs].sum(axis=1)
+    )
+    validate_predictions = p_HMM_predictor.predict_phenotype(
+        val[pbp_seqs].sum(axis=1)
+    )
+
+    results = ResultsContainer(
+        training_predictions=train_predictions,
+        validation_predictions=validate_predictions,
+        testing_predictions_1=test_predictions_1,
+        testing_predictions_2=test_predictions_2,
+        training_MSE=mean_squared_error(train_y, train_predictions),
+        validation_MSE=mean_squared_error(val_y, validate_predictions),
+        testing_MSE_1=mean_squared_error(test_1_y, test_predictions_1),
+        testing_MSE_2=mean_squared_error(test_2_y, test_predictions_2),
+        training_accuracy=accuracy(train_predictions, train_y),
+        validation_accuracy=accuracy(validate_predictions, val_y),
+        testing_accuracy_1=accuracy(test_predictions_1, test_1_y),
+        testing_accuracy_2=accuracy(test_predictions_2, test_2_y),
+        training_mean_acc_per_bin=mean_acc_per_bin(train_predictions, train_y),
+        validation_mean_acc_per_bin=mean_acc_per_bin(
+            validate_predictions, val_y
+        ),
+        testing_mean_acc_per_bin_1=mean_acc_per_bin(
+            test_predictions_1, test_1_y
+        ),
+        testing_mean_acc_per_bin_2=mean_acc_per_bin(
+            test_predictions_2, test_2_y
+        ),
+        hyperparameters={},
+        model_type="profile_HMM_phenotype_predictor",
+        model=p_HMM_predictor,
+        config={
+            "train_data_population": train_data_population,
+            "test_data_population_1": test_data_population_1,
+            "test_data_population_2": test_data_population_2,
+            "standardise_training_MIC": standardise_training_MIC,
+            "standardise_test_and_val_MIC": standardise_test_and_val_MIC,
+        },
+    )
+
+    print(results)
 
 
 if __name__ == "__main__":
--- models/HMM_model.py
+++ models/HMM_model.py
@@ -1,16 +1,64 @@
-from typing import List
+from operator import itemgetter
+from typing import Dict, Iterable, List
 
+import numpy as np
+import pandas as pd
 import pyhmmer
+from nptyping import NDArray
 from pyhmmer.plan7 import HMM
 
 
-def build_hmm(sequences: List[str]) -> HMM:
-    seqs = [
-        pyhmmer.easel.TextSequence(name=str(i).encode(), sequence=j)
-        for i, j in enumerate(sequences)
-    ]
-    alphabet = pyhmmer.easel.Alphabet.amino()
-    msa = pyhmmer.easel.TextMSA(name=b"msa", sequences=seqs).digitize(alphabet)
-    builder = pyhmmer.plan7.Builder(alphabet)
-    background = pyhmmer.plan7.Background(alphabet)
-    return builder.build_msa(msa, background)[0]
+class ProfileHMMPredictor:
+    def __init__(
+        self,
+        training_data: pd.DataFrame,
+        drop_duplicates_for_training: bool = True,
+        pbp_seqs: List[str] = ["a1_seq", "b2_seq", "x2_seq"],
+    ):
+        self.pbp_seqs = pbp_seqs
+        self.alphabet = pyhmmer.easel.Alphabet.amino()
+        self.background = pyhmmer.plan7.Background(self.alphabet, uniform=True)
+        self.builder = pyhmmer.plan7.Builder(self.alphabet)
+        self.pipeline = pyhmmer.plan7.Pipeline(
+            self.alphabet, background=self.background
+        )
+        self.hmm_mic_dict = self._phenotype_representative_HMMs(
+            training_data, drop_duplicates_for_training
+        )
+
+    def _build_hmm(self, sequences: Iterable[str], mic: float) -> HMM:
+        seqs = [
+            pyhmmer.easel.TextSequence(name=str(i).encode(), sequence=j)
+            for i, j in enumerate(sequences)
+        ]
+        msa = pyhmmer.easel.TextMSA(
+            name=str(mic).encode(), sequences=seqs
+        ).digitize(self.alphabet)
+        return self.builder.build_msa(msa, self.background)[0]
+
+    def _phenotype_representative_HMMs(
+        self,
+        data: pd.DataFrame,
+        unique_sequences: bool = True,
+    ) -> Dict:
+        hmm_mic_dict = {}  # type: ignore
+        for mic, sequences in data.groupby("log2_mic"):
+            sequences = sequences[self.pbp_seqs].sum(axis=1)
+            if unique_sequences:
+                sequences.drop_duplicates(inplace=True)
+            hmm_mic_dict[mic] = self._build_hmm(sequences, mic)
+        return hmm_mic_dict
+
+    def predict_phenotype(self, seqs: Iterable[str]) -> NDArray:
+        digital_sequences = [
+            pyhmmer.easel.TextSequence(sequence=i).digitize(self.alphabet)
+            for i in seqs
+        ]
+
+        def get_prediction(seq):
+            hits = self.pipeline.scan_seq(seq, self.hmm_mic_dict.values())
+            hits = [[hit.score, float(hit.name)] for hit in hits]
+            hits.sort(key=itemgetter(0))
+            return hits[-1][1]
+
+        return np.array([get_prediction(seq) for seq in digital_sequences])
