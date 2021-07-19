4d87194169b506f3cd6377541826a21894f305e7
Ben <bjeffrey92@hotmail.com>
2021-07-19 09:08:02 +0100

    functions to optimise hmc sampler with bayesopt

--- fit_ordinal_regression.py
+++ fit_ordinal_regression.py
@@ -1,11 +1,52 @@
-from typing import Optional
+import pickle
+from functools import partial
+from typing import Dict, List, Optional
 
-from fit_models import (
-    filter_features_by_previous_model_fit,
-    load_and_format_data,
-)
+from bayes_opt import BayesianOptimization
+from numpy import mean
+
+from fit_models import load_and_format_data
 from models import bayesian_ordinal_regression
 from utils import ordinal_regression_format
+
+
+def train_evaluate(
+    data: Dict,
+    beta_prior_sd: float,
+    num_warmup: int,
+    num_samples: int,
+    step_size: float,
+    target_accept_prob: float,
+) -> float:
+
+    model = bayesian_ordinal_regression.BayesianOrdinalRegression(
+        data["train"][0],
+        data["train"][1],
+        data["train"][0].shape[1],
+        len(set(data["train"][1])),
+        beta_prior_sd=beta_prior_sd,
+        num_chains=2,
+    )
+    model.fit_with_NUTS(
+        num_warmup=int(num_warmup),
+        num_samples=int(num_samples),
+        step_size=step_size,
+        target_accept_prob=target_accept_prob,
+    )
+    gr_stats = model.gelman_rubin_stats()
+
+    return -mean(list(gr_stats.values()))
+
+
+def optimise_HMC_sampler(
+    data: Dict, pbounds=Dict[str, List[float]], init_points=5, n_iter=15
+):
+    partial_fitting_function = partial(train_evaluate, data=data)
+    optimizer = BayesianOptimization(
+        f=partial_fitting_function, pbounds=pbounds, random_state=0
+    )
+    optimizer.maximize(init_points=init_points, n_iter=n_iter)
+    return optimizer
 
 
 def main(
@@ -41,26 +82,17 @@ def main(
         standardise_test_and_val_MIC=standardise_test_and_val_MIC,
         extended_sequences=extended_sequences,
     )
-    data = ordinal_regression_format(data, minor_variant_frequency)
-
-    # filter features by things which have been used by previously fitted model
-    if previous_rf_model is not None:
-        data = filter_features_by_previous_model_fit(previous_rf_model, data)
-
-    model = bayesian_ordinal_regression.BayesianOrdinalRegression(
-        data["train"][0],
-        data["train"][1],
-        data["train"][0].shape[1],
-        len(set(data["train"][1])),
-        beta_prior_sd=1.0,
-        num_chains=2,
-    )
-    model.fit(num_warmup=10000, num_samples=100000)
-    print(model.test_convergence())
-
-    bayesian_ordinal_regression.save_bayesian_ordinal_regression(
-        model, "bayesian_ordinal_regression.pkl"
-    )
+    data = ordinal_regression_format(data, 0.05)
+    pbounds = {
+        "beta_prior_sd": [0.1, 10.0],
+        "num_warmup": [10000, 100000],
+        "num_samples": [10000, 100000],
+        "step_size": [0.8, 1.2],
+        "target_accept_prob": [0.5, 0.7],
+    }
+    optimizer = optimise_HMC_sampler(data, pbounds=pbounds)
+    with open("hmc_sampler_optimizer.pkl", "wb") as f:
+        pickle.dump(optimizer, f)
 
 
 if __name__ == "__main__":
