0c0458061473b7e7281bee6ca78d99e957b1a039
Ben <bjeffrey92@hotmail.com>
2022-02-28 11:42:07 +0000

    more metric recording

--- fit_phylogeny_GNN_model.py
+++ fit_phylogeny_GNN_model.py
@@ -1,57 +1,93 @@
 import time
+from typing import Tuple
 
-import torch.optim as optim
+import numpy as np
+import torch
 import torch.nn.functional as F
 
 from models.phylogeny_GNN_model import GCN
 from parse_GCN_data import load_data
-from utils import accuracy
+from utils import accuracy, mean_acc_per_bin
 
 
-EPOCHS = 100
-
+EPOCHS = 150
 
 data = load_data(filter_constant_features=True)
 X = data["X"]
 y = data["y"]
+y_np = np.squeeze(y.numpy())
 adj = data["adj"]
 idx_train, idx_val, idx_test = data["CV_indices"]
 
-model = GCN(X.shape[1], 100, 1, 0.3)
-optimizer = optim.Adam(model.parameters())
+model = GCN(X.shape[1], X.shape[1], 250, 1, 0.3)
+optimizer = torch.optim.Adam(model.parameters())
+
+metrics_dict = {
+    "train_loss": [],
+    "train_acc": [],
+    "train_mean_bin_acc": [],
+    "val_loss": [],
+    "val_acc": [],
+    "val_mean_bin_acc": [],
+    "test_loss": [],
+    "test_acc": [],
+    "test_mean_bin_acc": [],
+}
 
 
-def train(epoch):
+def compute_metrics(
+    output: torch.Tensor, output_np: np.ndarray, data_set: str
+) -> Tuple:
+    if data_set == "train":
+        idx = idx_train
+    elif data_set == "test":
+        idx = idx_test
+    elif data_set == "val":
+        idx = idx_val
+
+    loss = F.mse_loss(output[idx], y[idx])
+    acc = accuracy(output[idx], y[idx])
+    mean_bin_acc = mean_acc_per_bin(output_np[idx], y_np[idx])
+
+    metrics_dict[f"{data_set}_loss"].append(loss.item())
+    metrics_dict[f"{data_set}_acc"].append(acc)
+    metrics_dict[f"{data_set}_mean_bin_acc"].append(mean_bin_acc)
+
+    return loss, acc, mean_bin_acc
+
+
+def train(epoch: int):
     t = time.time()
     model.train()
     optimizer.zero_grad()
     output = model(X, adj)
-    loss_train = F.mse_loss(output[idx_train], y[idx_train])
-    acc_train = accuracy(output[idx_train], y[idx_train])
+    output_np = np.squeeze(output.detach().numpy())
+    loss_train, acc_train, mean_acc_train = compute_metrics(output, output_np, "train")
     loss_train.backward()
     optimizer.step()
-
-    loss_val = F.mse_loss(output[idx_val], y[idx_val])
-    acc_val = accuracy(output[idx_val], y[idx_val])
+    loss_val, acc_val, mean_acc_val = compute_metrics(output, output_np, "val")
     print(
         f"Epoch: {epoch + 1}",
         f"loss_train: {loss_train.item()}",
         f"acc_train: {acc_train}",
+        f"mean_acc_train: {mean_acc_train}",
         f"loss_val: {loss_val.item()}",
         f"acc_val: {acc_val}",
-        f"time: {time.time() - t}s",
+        f"mean_acc_val: {mean_acc_val}",
+        f"time: {time.time() - t}s\n",
     )
 
 
 def test():
     model.eval()
     output = model(X, adj)
-    loss_test = F.mse_loss(output[idx_test], y[idx_test])
-    acc_test = accuracy(output[idx_test], y[idx_test])
+    output_np = np.squeeze(output.detach().numpy())
+    loss_test, acc_test, mean_acc_test = compute_metrics(output, output_np, "test")
     print(
         "Test set results:",
-        f"loss= {loss_test.item()}",
-        f"accuracy= {acc_test}",
+        f"loss_test = {loss_test.item()}",
+        f"acc_test = {acc_test}",
+        f"mean_acc_test = {mean_acc_test}\n",
     )
 
 
@@ -59,8 +95,6 @@ if __name__ == "__main__":
     t_total = time.time()
     for epoch in range(EPOCHS):
         train(epoch)
+        test()
     print("Optimization Finished!")
     print(f"Total time elapsed: {time.time() - t_total}s")
-
-    # Testing
-    test()
