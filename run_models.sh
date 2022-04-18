bb44487beb356c20418e620c00cc2967276626cb
Ben <bjeffrey92@hotmail.com>
2022-04-18 13:03:38 +0100

    training loop for the supervised and unsupervised models

--- run_models.sh
+++ run_models.sh
@@ -2,23 +2,19 @@
 
 fit_model () {
     # run with testing populations in each order
+
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --blosum_inference 
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --blosum_inference
+
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --HMM_inference 
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --HMM_inference
+
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --HMM_MIC_inference 
+    ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --HMM_MIC_inference
+
     ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --blosum_inference --just_HMM_scores
     ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --blosum_inference --just_HMM_scores
 
-    if [[ "$1" != "random_forest" ]]
-    then
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --blosum_inference 
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --blosum_inference
-
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --HMM_inference 
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --HMM_inference
-
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --HMM_MIC_inference 
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --HMM_MIC_inference
-
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $3 --test_pop_2 $4 --blosum_inference --just_HMM_scores
-        ./fit_models.py --model_type $1 --train_pop $2 --test_pop_1 $4 --test_pop_2 $3 --blosum_inference --just_HMM_scores
-    fi
 }
 
 POPS=("cdc" "pmen" "maela")
@@ -28,11 +24,9 @@ do
     test_pop_1=${TEST_POPS[@]:0:1}
     test_pop_2=${TEST_POPS[@]:1:1}
 
+    fit_model "random_forest" $training_pop $test_pop_1 $test_pop_2
+    fit_model "elastic_net" $training_pop $test_pop_1 $test_pop_2
     fit_model "DBSCAN" $training_pop $test_pop_1 $test_pop_2
     fit_model "DBSCAN_with_UMAP" $training_pop $test_pop_1 $test_pop_2
 
 done
-
-
-# TODO: random forest for everything with just hmm scores and hmm scores included
-# redo DBSCAN and DBSCAN with UMAP for everything
