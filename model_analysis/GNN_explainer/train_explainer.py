d0a1e67cae9b4c6f4c733788ce667070f5df451a
Ben <bjeffrey92@hotmail.com>
2022-05-11 09:15:54 +0100

    explanations script for all nodes

--- model_analysis/GNN_explainer/train_explainer.py
+++ model_analysis/GNN_explainer/train_explainer.py
@@ -1,10 +1,19 @@
+import logging
+import os
+import pickle
 import tempfile
-from typing import List
+from tqdm import tqdm
+from typing import Dict, List
 
 import numpy as np
 import torch
 
-from .simplified_explainer import ExplainModule, load_neighborhoods
+from parse_GCN_data import load_data
+from models.phylogeny_GNN_model import GCN
+from model_analysis.GNN_explainer.simplified_explainer import (
+    ExplainModule,
+    load_neighborhoods,
+)
 
 
 def extract_neighborhood(node_idx, adj, feat, labels, neighborhoods):
@@ -25,7 +34,7 @@ def _build_explainer(x, adj, model, label) -> ExplainModule:
     optimiser_params = {
         "lr": 0.5,
         "opt_scheduler": "step",
-        "opt_decay_step": 200,
+        "opt_decay_step": 50,
         "opt_decay_rate": 0.5,
         "weight_decay": 0,
     }
@@ -48,8 +57,9 @@ def train_explainer(
     label,
     node_idx_new: int,
     pred_label: float,
-    num_epochs: int = 500,
+    num_epochs: int = 200,
     unconstrained: bool = False,
+    verbose=False,
 ) -> ExplainModule:
     """
     Trains the explainer module to predict one node in the graph
@@ -72,21 +82,21 @@ def train_explainer(
             explainer.scheduler.step()
         mask_density = explainer.mask_density()
 
-        loss_value = explainer.loss(ypred, pred_label, node_idx_new)
-        losses.append(loss_value)
-        if loss[-1] == min(losses):
+        losses.append(loss)
+        if losses[-1] == min(losses):
             torch.save(explainer.state_dict(), state_dict_file.name)
 
-        print(
-            "epoch: ",
-            epoch,
-            "; loss: ",
-            loss.item(),
-            "; mask density: ",
-            mask_density.item(),
-            "; pred: ",
-            ypred,
-        )
+        if verbose:
+            print(
+                "epoch: ",
+                epoch,
+                "; loss: ",
+                loss.item(),
+                "; mask density: ",
+                mask_density.item(),
+                "; pred: ",
+                ypred,
+            )
 
     optimal_state = torch.load(state_dict_file.name)
     explainer.load_state_dict(optimal_state)
@@ -94,11 +104,55 @@ def train_explainer(
     return explainer
 
 
-def save_explanations(explainer: ExplainModule, node_idx: int):
-    print(f"Node {node_idx} feature_mask: ")
-    print(explainer.feat_mask)
-    print(f"Node {node_idx} adjacency mask: ")
-    print(explainer.masked_adj)
+def save_explanations(node_explanations: Dict[int, np.ndarray]):
+    ...
+
+
+def load_model_and_data(
+    filename: str, basepath: str = "results/phylogeny_GNN_model/hamming_dist_tree/"
+):
+    fpath = os.path.join(basepath, filename)
+    with open(fpath, "rb") as a:
+        result = pickle.load(a)
+
+    laplacian = True
+    hamming_dist_network = False
+    tree = False
+    n_duplicates_hd_network = False
+    hamming_dist_tree = True
+    drop_duplicates = False
+    rank_hamming_distance = False
+    hd_cuttoff = 5
+    n = 3
+
+    logging.warning("data being loaded with default arguments")
+
+    data = load_data(
+        filter_constant_features=True,
+        train_population=result["train_population"],
+        test_population_1=result["test_population_1"],
+        graph_laplacian=laplacian,
+        tree=tree,
+        n_duplicates_hd_network=n_duplicates_hd_network,
+        hamming_dist_tree=hamming_dist_tree,
+        hamming_dist_network=hamming_dist_network,
+        drop_duplicates=drop_duplicates,
+        ranked_hamming_distance=rank_hamming_distance,
+        hd_cuttoff=hd_cuttoff,
+        n=n,
+    )
+    X = data["X"]
+    y = data["y"]
+    adj = data["laplacian"] if laplacian else data["adj"]
+    idx_train = data["CV_indices"][0]
+
+    adj = adj.to_dense()
+    model = GCN(
+        X.shape[1], X.shape[1], **result["model_params"], nclass=1, sparse_adj=False
+    )
+    model.load_state_dict(result["model_state_dict"])
+
+    return X, y, adj, idx_train, model
 
 
 def main(
@@ -106,13 +160,21 @@ def main(
     adj: torch.Tensor,
     feat: torch.Tensor,
     labels: torch.Tensor,
-    predictions: torch.Tensor,
     num_gc_layers: int,
-    node_indices: List[int] = [],
+    node_indices: List[int],
 ):
+
+    predictions = model(X, adj).detach()
+
     neighborhoods = load_neighborhoods(adj, n_hops=num_gc_layers)
-    predictions = predictions.detach()
-    for node_idx in node_indices:
+
+    def extract_explanations(
+        node_idx: int,
+        adj: torch.Tensor,
+        feat: torch.Tensor,
+        labels: torch.Tensor,
+        neighborhoods: np.ndarray,
+    ) -> np.ndarray:
         neighbours_adj, x, label, node_idx_new, neighbors = extract_neighborhood(
             node_idx, adj, feat, labels, neighborhoods
         )
@@ -120,4 +182,19 @@ def main(
         explainer = train_explainer(
             x, neighbours_adj, model, label, node_idx_new, pred_label
         )
-        save_explanations(explainer, node_idx)
+        feature_differences = (explainer.mask_features() - x).abs().mean(0)
+        return feature_differences.detach().numpy()
+
+    return {
+        node_idx: extract_explanations(node_idx, adj, feat, labels, neighborhoods)
+        for node_idx in tqdm(node_indices, "Getting explanatory features per node")
+    }
+
+
+if __name__ == "__main__":
+    basepath = "results/phylogeny_GNN_model/hamming_dist_tree/"
+    for filename in os.listdir(basepath):
+        X, y, adj, idx_train, model = load_model_and_data(filename)
+        node_explanations = main(
+            model, adj, X, y, num_gc_layers=3, node_indices=idx_train
+        )
