b01a74be0af8454e8ec9e93b6a81bd91281002a3
Ben <bjeffrey92@hotmail.com>
2022-05-20 11:01:59 +0100

    plot dist of MIC values in each dbscan cluster

---
+++ model_analysis/DBSCAN_analysis.py
@@ -0,0 +1,74 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def get_clusters(fpath: str) -> pd.DataFrame:
+    with open(fpath, "rb") as a:
+        results = pickle.load(a)
+    clusters_df = pd.DataFrame(
+        {
+            "MIC": results.training_predictions,
+            "cluster_labels": results.model.clustering.labels_,
+        }
+    )
+    clusters_df = clusters_df.loc[clusters_df.cluster_labels != -1]  # drop unassigned
+    median_cluster_MIC = clusters_df.groupby("cluster_labels").apply(
+        lambda x: x.median()
+    )
+    df = clusters_df.join(median_cluster_MIC, on="cluster_labels", rsuffix="_median")
+    df = df.sort_values("MIC_median")
+    df = df.assign(
+        **{
+            "cluster_labels": pd.factorize(df.cluster_labels_median)[0].astype(str),
+            "Test Population 1": results.config["test_1_population"],
+        }
+    )
+    return df[["MIC", "cluster_labels", "Test Population 1"]].reset_index(drop=True)
+
+
+def plot_MIC_cluster_distribution(df: pd.DataFrame, fname: str):
+    plt.rcParams.update({"font.size": 16})
+    g = sns.catplot(
+        data=df,
+        x="cluster_labels",
+        y="MIC",
+        hue=None,
+        col="Test Population 1",
+        row="Model",
+        kind="box",
+        sharex=False,
+    )
+    g.set_xticklabels([])
+    g.set_ylabels("log2(MIC)")
+    g.set_xlabels("Cluster")
+    g.savefig(fname)
+
+
+def main():
+    result_files = [
+        [
+            "results/DBSCAN/train_pop_cdc_results_no_inference_pbp_types.pkl",
+            "DBSCAN",
+        ],
+        [
+            "results/DBSCAN/train_pop_cdc_results_no_inference_pbp_types(1).pkl",
+            "DBSCAN",
+        ],
+        [
+            "results/DBSCAN_with_UMAP/train_pop_cdc_results_no_inference_pbp_types.pkl",
+            "DBSCAN-UMAP",
+        ],
+        [
+            "results/DBSCAN_with_UMAP/train_pop_cdc_results_no_inference_pbp_types(1).pkl",
+            "DBSCAN-UMAP",
+        ],
+    ]
+    df = pd.concat([get_clusters(i[0]).assign(Model= i[1]) for i in result_files])
+    plot_MIC_cluster_distribution(df)
+
+
+if __name__ == "__main__":
+    main()
